24e639f96ba00dc09f4cb7d75509d815d8484dd9
Max Blancarte <blancarte@gmail.com>
2021-10-30 17:42:21 -0700

    adding users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
 import * as Joi from '@hapi/joi';
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import * as Joi from '@hapi/joi';
       }),
     }),
     DatabaseModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
